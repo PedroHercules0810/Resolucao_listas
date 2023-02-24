9751ea1b49d04875eb440eae05d2a4162f7d0c38
Pedro Hércules <46134763+PedroHercules0810@users.noreply.github.com>
2023-02-23 22:24:23 -0300

    Add files via upload

--- ALOCAÇÃO DINIMICA DE VETORES/questao1.c
+++ ALOCAÇÃO DINIMICA DE VETORES/questao1.c
@@ -1,43 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main()
+{
+    // criando as variáveis
     int N, i;
     char *sexo;
     char *opiniao;
     int contF = 0, contMF = 0, contNM = 0;
     float porcF, porcM;
 
+    // pedindo para o usuário informar o numero de pessoas
     printf("Informe o numero de pessoas entrevistadas: ");
     scanf("%d", &N);
 
-    sexo = (char *) malloc(N * sizeof(char));
-    opiniao = (char *) malloc(N * sizeof(char));
+    // alocando dinamicamente o sexo e a opinão de acordo com N
+    sexo = (char *)malloc(N * sizeof(char));
+    opiniao = (char *)malloc(N * sizeof(char));
 
-    for (i = 0; i < N; i++) {
-        printf("Informe o sexo (M/F) da pessoa %d: ", i+1);
+    // usando laço de repetição for para informar o sexo das pessoas e a opinião
+    for (i = 0; i < N; i++)
+    {
+        printf("Informe o sexo (M/F) da pessoa %d: ", i + 1);
         scanf(" %c", &sexo[i]);
 
-        printf("Informe a opiniao (g = gostou/ n = nao gostou) do produto da pessoa %d: ", i+1);
+        printf("Informe a opiniao (g = gostou/ n = nao gostou) do produto da pessoa %d: ", i + 1);
         scanf(" %s", &opiniao[i]);
     }
 
-    for (i = 0; i < N; i++) {
-        if (sexo[i] == 'F' && opiniao[i] == 'g') {
+    // usando for para fazer a contagem
+    for (i = 0; i < N; i++)
+    {
+        if (sexo[i] == 'F' && opiniao[i] == 'g')
+        {
+            // se for do sexo Feminino e a opção for que gostou, incrementa a variavel
             contF++;
-        } else if (sexo[i] == 'F' && opiniao[i] == 'n') {
+        }
+        else if (sexo[i] == 'F' && opiniao[i] == 'n')
+        {
+            // se for do sexo feminino e a opção for que não gostou, incrementa outra variavel
             contMF++;
-        } else if (sexo[i] == 'M' && opiniao[i] == 'n') {
+        }
+        else if (sexo[i] == 'M' && opiniao[i] == 'n')
+        {
+            // se o sexxo for masculino e a opção for que não gostou, incrementa outra variavel
             contNM++;
         }
     }
 
-    porcF = (float) contF / (N / 100.0);
-    porcM = (float) contNM / (N / 100.0);
+    // canculando as porcentagens
+    porcF = (float)contF / (N / 100.0);
+    porcM = (float)contNM / (N / 100.0);
 
+    // mostrando as porcentagens
     printf("\nPorcentagem de mulheres que gostaram do produto: %.2f%%\n", porcF);
     printf("Porcentagem de homens que nao gostaram do produto: %.2f%%\n", porcM);
 
+    //liberando a memória
     free(sexo);
     free(opiniao);
 
--- ALOCAÇÃO DINIMICA DE VETORES/questao2.c
+++ ALOCAÇÃO DINIMICA DE VETORES/questao2.c
@@ -1,54 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main()
+{
+    // criando as variaveis
     int n_questoes, i, j;
     char gabarito[100], **respostas;
     float nota[10], nota_minima = 6.0, porcentagem_aprovados;
     int aprovados = 0, reprovados = 0;
 
+    // pedindo para o usuario informar o numero de questões e o gabarito
     printf("Digite o número de questões: ");
     scanf("%d", &n_questoes);
     printf("Digite o gabarito da prova: ");
     scanf("%s", gabarito);
 
-    respostas = (char **) malloc(10 * sizeof(char *));
-    if (respostas == NULL) {
+    // alocando dinamicamente as respostas como ponteiro de ponteiro
+    respostas = (char **)malloc(10 * sizeof(char *));
+    if (respostas == NULL)
+    {
+        // se a alocação falhar emite a mensagem de erro e fecha o programa
         printf("Erro ao alocar memória!");
         exit(1);
     }
-    for (i = 0; i < 10; i++) {
-        respostas[i] = (char *) malloc(n_questoes * sizeof(char));
-        if (respostas[i] == NULL) {
+
+    // usando laço de repetição para alocar dinamicamente as respostas de acordo com o numero de questões
+    for (i = 0; i < 10; i++)
+    {
+        respostas[i] = (char *)malloc(n_questoes * sizeof(char));
+        if (respostas[i] == NULL)
+        {
             printf("Erro ao alocar memória!");
             exit(1);
         }
     }
 
-    for (i = 0; i < 10; i++) {
-        printf("Digite as respostas do aluno %d: ", i+1);
+    // usando um for para passar o gabarito dos alunos
+    for (i = 0; i < 10; i++)
+    {
+        printf("Digite as respostas do aluno %d: ", i + 1);
         scanf("%s", respostas[i]);
 
         nota[i] = 0;
-        for (j = 0; j < n_questoes; j++) {
-            if (respostas[i][j] == gabarito[j]) {
+        // usando outro for para comparar as respostas do aluno com o gabarito e calculando a média do aluno
+        for (j = 0; j < n_questoes; j++)
+        {
+            if (respostas[i][j] == gabarito[j])
+            {
                 nota[i] += 10.0 / n_questoes;
             }
         }
-        printf("Nota do aluno %d: %.2f\n", i+1, nota[i]);
+        printf("Nota do aluno %d: %.2f\n", i + 1, nota[i]);
 
-        if (nota[i] >= nota_minima) {
+        // se a nota for maior do que a nota minima, a variavel aprovados é incrementada
+        if (nota[i] >= nota_minima)
+        {
             aprovados++;
-        } else {
+        }
+        else
+        {
+            // se não a quantidade de reprovados é incrementada
             reprovados++;
         }
     }
 
+    // calculando a porcentagem de aprovação e printando ela
     porcentagem_aprovados = 100.0 * aprovados / (aprovados + reprovados);
 
     printf("Porcentagem de aprovados: %.2f%%\n", porcentagem_aprovados);
 
-    for (i = 0; i < 10; i++) {
+    // liberando a memória
+    for (i = 0; i < 10; i++)
+    {
         free(respostas[i]);
     }
     free(respostas);
